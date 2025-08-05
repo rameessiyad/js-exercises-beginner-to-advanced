6c5500c5c4f0ad5d1010127a9e101bc23005166b
rameessiyad <rameesmsiyadofficial786@gmail.com>
2025-08-05 07:34:56 +0530

    All qs

--- exercies/numbersAndMath.js
+++ exercies/numbersAndMath.js
@@ -8,9 +8,13 @@ let costPerPerson = costPerOrder / 3 //-> cost per person
 
 
 //3 calculate the total cost of a toaster ($18.50) and 2 shirts ($7.50 each).
-console.log(18.50 + 2 * 7.50)
+console.log(18.50 + 2 * 7.50, "total cost of 2 toasters and 2 shirts")
 
-//4 calculate a 10% tax fot the total in above exercise
+//4 calculate a 10% tax for the total in above exercise
+let totalCost = 18.50 + 2 * 7.50;
+let tax = totalCost * 0.1; //-> 10% tax -> 10/100 -> 0.1
+console.log(tax, "total cost of 2 toasters and 2 shirts with 10% tax")
 
-
-//5 calculate a 20% tax fot the total in 2c (remember that 1% = 1/100, so 20% is 20/100 = 0.2)
+//5 calculate a 20% tax fot the total in above question (remember that 1% = 1/100, so 20% is 20/100 = 0.2)
+let twentyPercentTax = totalCost * 0.2;
+console.log(twentyPercentTax, "total cost of 2 toasters and 2 shirts with 20% tax")
