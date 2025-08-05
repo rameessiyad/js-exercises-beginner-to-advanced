395bb967c4a8f6f12358c3ba468b56feb709b450
rameessiyad <rameesmsiyadofficial786@gmail.com>
2025-08-05 07:21:01 +0530

    initial commit

---
+++ exercies/numbersAndMath.js
@@ -0,0 +1,9 @@
+//1 At a restaurant, you order 1 soup for $10, 3 burgers for $8 each, and 1 ice cream for $5, calcuate the cost of the order.
+// console.log
+(10 + 3 * 8 + 5);
+
+//2 you're at the restaurant  with 2 friends (3 people in total) and make the same order as question 1, calculate how much each person pays
+let costPerOrder = 10 + 3 * 8 + 5;
+let costPerPerson = costPerOrder / 3
+
+console.log("cost per person is : $",constPerPerson)
