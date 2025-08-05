962156a4f8a31753e1e6837875c8365d4d3df124
rameessiyad <rameesmsiyadofficial786@gmail.com>
2025-08-05 22:38:28 +0530

    1st question

---
+++ exercies/strings.js
@@ -0,0 +1,6 @@
+//1 At a restaurant, you order 1 coffee($5) and 1 bagel($3). Using math, calculate the total cost,
+// and using concatenation, create the text 'Total cost: $__'(replace with the total you calculated).
+
+let totalCost = 5 + 3;
+let text = "Total cost: $" + totalCost;
+console.log(text);
