6c5026927f6673cf348b3b2b6b9d0b9b7da143a2
rameessiyad <rameesmsiyadofficial786@gmail.com>
2025-08-05 07:31:28 +0530

    3rd qs

--- exercies/numbersAndMath.js
+++ exercies/numbersAndMath.js
@@ -4,6 +4,13 @@
 
 //2 you're at the restaurant  with 2 friends (3 people in total) and make the same order as question 1, calculate how much each person pays
 let costPerOrder = 10 + 3 * 8 + 5;
-let costPerPerson = costPerOrder / 3
+let costPerPerson = costPerOrder / 3 //-> cost per person
 
-console.log("cost per person is : $",constPerPerson)
+
+//3 calculate the total cost of a toaster ($18.50) and 2 shirts ($7.50 each).
+console.log(18.50 + 2 * 7.50)
+
+//4 calculate a 10% tax fot the total in above exercise
+
+
+//5 calculate a 20% tax fot the total in 2c (remember that 1% = 1/100, so 20% is 20/100 = 0.2)
