ffceafc0a533118cd992708ce21ce2314ba1bca7
rameessiyad <rameesmsiyadofficial786@gmail.com>
2025-08-11 07:01:56 +0530

    To do list

--- exercies/Array&Loops/index.js
+++ exercies/Array&Loops/index.js
@@ -1,2 +1,8 @@
 const myArr = [10, 20, "hello", true, { name: "Ramees" }, [55, 34]];
-console.log(myArr);
+// console.log(myArr);
+// console.log(typeof(myArr))
+console.log(Array.isArray(myArr));
+
+myArr.push(100);
+myArr.splice(0, 2);
+console.log(myArr)
