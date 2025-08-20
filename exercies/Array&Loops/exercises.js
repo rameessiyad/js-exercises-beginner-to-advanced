5d184ccd68cadd1dfef7f647aa8fa3805a249412
rameessiyad <rameesmsiyadofficial786@gmail.com>
2025-08-20 22:15:32 +0530

    q4 answer

--- exercies/Array&Loops/exercises.js
+++ exercies/Array&Loops/exercises.js
@@ -20,4 +20,10 @@ const arraySwap = (array) => {
   console.log(arr);
 };
 
-arraySwap(["hi", "hello", "good"]);
+// arraySwap(["hi", "hello", "good"]);
+
+//q-4 create a loop that counts up from 0 to 10, but counts up by 2 (0,2,4,...,8,10);
+for (let i = 0; i <= 10;) {
+  console.log(i);
+  i= i+2
+}
