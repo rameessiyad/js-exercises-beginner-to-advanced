a97cb81ee93d199f493ae20a1c49b0280ba8dc98
rameessiyad <rameesmsiyadofficial786@gmail.com>
2025-08-20 21:48:40 +0530

    q2

--- exercies/Array&Loops/exercises.js
+++ exercies/Array&Loops/exercises.js
@@ -1,4 +1,8 @@
 //q-1 Create an array of numbers: const nums = [10,20,30]; Modify the last value in this array to 99
 const nums = [10, 20, 30];
 nums[nums.length - 1] = 99;
-console.log(nums);
+// console.log(nums);
+
+//q-2 create a func getLastValue(array) that takes an array and return its last value;
+
+
